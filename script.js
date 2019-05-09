8e5546e002ad66d8003c9e39a07d8d2facf16906
Bruger <bruger@Brugers-MacBook-Air.local>
2019-05-09 08:51:43 +0200

    hjemmeside iframe ikke ok

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict'
+
+//Toggle animation burger menu button
+document.querySelector(".burger").addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+	document.querySelector(".burger").classList.toggle("change");
+	document.querySelector("nav").classList.toggle("show");
+}
